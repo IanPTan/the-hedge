de98c2a0d33417a69922f5d181db3fbaba729bc1
Ian Titor <ianpatricktan@gmail.com>
2024-11-25 16:44:46 -0500

    ticker scraper functional now but not optimal or integrated with the database

--- src/ticker.py
+++ src/ticker.py
@@ -1,79 +1,84 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests as rq
+from lxml import html
+from time import time
+
+driver_path = "/usr/bin/geckodriver"
+service = Service(driver_path)
+options = Options()
+#options.add_argument("--headless")
+driver = webdriver.Firefox(service=service, options=options)
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
+
+url = "https://www.google.com/search?q=fda+site:https://finance.yahoo.com/news/&sca_esv=a3aa32e04255f5fb&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwif45GVl_iJAxUDmokEHdgPDogQpwV6BAgFEAo&biw=1707&bih=944&dpr=1.5"
+xpaths = {
+        "results": "/html/body/div[3]/div/div[13]/div/div[2]/div[2]/div/div/div/div/div/div/div[1]/div/div/span/a | /html/body/div[3]/div/div[13]/div/div[2]/div[2]/div/div/div/div/div/div[1]/div/div/span/a",
+        "next": "/html/body/div[3]/div/div[13]/div/div[4]/div/div[3]/table/tbody/tr/td[12]/a",
+        "tickers": "/html/body/div[2]/main/section/section/section/article/div/div[1]/div[3]/div[1]/div/div/div/div/div/a/div/span",
+        "robot": "/html/body/div[1]/div/b",
+        }
 
 
-driver = webdriver.Chrome()  # Replace with your preferred browser
+def ticker_scan(url, xpaths=xpaths, headers=headers):
 
-def extract_article_urls(url):
-    text_list = []
+    page = rq.get(url, headers=headers)
+
+    if page.status_code != 200:
+        raise ValueError(f"\"{url}\" failed to load: ERROR {page.status_code}")
+
+    return [element.text.strip() for element in html.fromstring(page.text).xpath(xpaths["tickers"])]
+
+
+def raw_google_scan(driver, url, xpaths=xpaths):
+
     driver.get(url)
-    restart = True
-    while restart==True:
-        # takes all of the article links
-        links = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, "/html/body/div[3]/div/div[13]/div/div[2]/div[2]/div/div/div/div/div/div[1]/div/div/span/a"))  # xpath for the article
-        )
+    results = []
+    while 1:
 
-        article_urls = []
-        #finds all of the links to the articles
+        if driver.find_elements(By.XPATH, xpaths["robot"]):
+            if pause:
+                _ = input("Captcha detected! Completed?: ")
+            else:
+                return tickers
 
-        for link in links:
-            article_url = link.get_attribute('href')
-            article_urls.append(article_url)
-        #open text list
+        results += [element.get_dom_attribute("href") for element in driver.find_elements(By.XPATH, xpaths["results"])]
 
-        for article_url in article_urls:
-            # Open a new tab
-            driver.execute_script("window.open('');")
-            #opens up the given article
-
-            # switch to the new tab
-            window_after = driver.window_handles[1]
-            driver.switch_to.window(window_after)
-
-            driver.get(article_url)
-
-            if check_xpath_existence(driver,"/html/body/div[2]/main/section/section/section/article/div/div[1]/div[3]/div[1]/div/div/div/div/div/a/div/span"):
-                # the xpath exists
-                element = WebDriverWait(driver, 20).until(
-                    EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/main/section/section/section/article/div/div[1]/div[3]/div[1]/div/div/div/div/div/a/div/span"))
-                )
-                text = element.text.strip()
-                if text not in text_list:
-                    text_list.append(text)
-
-            #closes the tab
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
-
-            #Code to see if there is a next page
-        if check_xpath_existence(driver,"/html/body/div[3]/div/div[13]/div/div[4]/div/div[3]/table/tbody/tr/td[12]/a" ):
-            # Find the "Next" button
-            next_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div/div[13]/div/div[4]/div/div[3]/table/tbody/tr/td[12]/a")))
-            # Click the "Next" button
-            next_button.click()
+        next = driver.find_elements(By.XPATH, xpaths["next"])
+        if next:
+            next[0].click()
+            if driver.find_elements(By.XPATH, xpaths["robot"])[0].text == "About this page" and pause:
+                _ = input("Captcha detected! Completed?: ")
+                continue
         else:
-            return text_list
+            return results
 
 
-#checks if it exists
-def check_xpath_existence(driver, xpath):
-    try:
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, xpath))
-        )
-        return True
-    except:
-        return False
+def google_scan(driver, url, xpaths=xpaths, headers=headers, pause=1):
 
-# Example usage:
-url = "https://www.google.ca/search?q=site:finance.yahoo.com+%22FDA%22&sca_esv=69b2d222702df3a7&source=lnms&fbs=AEQNm0CrHVBV9axs7YmgJiq-TjYc7RgyMjmhctvLCnk5YpVfOzTk9UgrCkq1LL6wECoQ_WHzfZcSELpXaEiujjL7Q6oq7xqg_ok_kSsBaDM483VRK4lyvIYAvA84z9GW-6XFz0AOQRHB2zMuinHUAngvDSQ835MgWH4RST5egs07mXF9v4oFWz17sYS05cF7cYs8Af6knIYVcIflpfPTXDGSq6aOvTMJ5Q&sa=X&ved=2ahUKEwj_vvqso7-JAxWThIkEHT2iMCMQ0pQJegQIBhAD&biw=1440&bih=669&dpr=1"
-text_list = extract_article_urls(url)
+    driver.get(url)
+    tickers = set()
+    while 1:
 
-for text in text_list:
-    print(text)
-#for url in article_urls:
-  #print(url)
+        for element in driver.find_elements(By.XPATH, xpaths["results"]):
+            article_url = element.get_dom_attribute("href")
+            try:
+                tickers = tickers.union(set(ticker_scan(article_url, xpaths, headers)))
+                print(f"Successfully scraped: {article_url}")
+            except:
+                print(f"Failed to scrape: {article_url}")
 
+        next = driver.find_elements(By.XPATH, xpaths["next"])
+        if next:
+            next[0].click()
+        else:
+            robot_element = driver.find_elements(By.XPATH, xpaths["robot"])
+            if robot_element and robot_element[0].text == "About this page" and pause:
+                _ = input("Captcha detected! Completed?: ")
+                continue
+            return tickers
+
+
+results = google_scan(driver, url, xpaths, headers, 1)
