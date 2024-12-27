410500581ce00ba4d11388c659b3ea9d22e0a733
IanPatrickMTan <ianparicktan@gmail.com>
2024-12-27 06:15:59 -0500

    changed the month scan to just a day url generator since they aren't that difficult to generate, also updated the scan day function to exit properly now and to also retry

--- src/ticker.py
+++ src/ticker.py
@@ -1,15 +1,12 @@
 import requests as rq
+import pandas as pd
 from lxml import html
 from time import time
-import pandas as pd
+import datetime
 
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
-
-
 INDEX_URL = "https://finance.yahoo.com/sitemap"
-
-
 XPATHS = {
         "months": "/html/body/div[1]/div/main/div/div/div/ul/li/ul/li/a",
         "days": "/html/body/div[1]/div/main/div/div/div/div[1]/div/a",
@@ -18,6 +15,16 @@ XPATHS = {
         "tickers": "/html/body/div[2]/main/section/section/section/article/div/div[1]/div[3]/div[1]/div/div/div/div/div/a/div/span",
         "time": "/html/body/div[2]/main/section/section/section/article/div/div[1]/div[2]/div[1]/div/div[2]/time"
         }
+
+
+day_url = lambda date: f"https://finance.yahoo.com/sitemap/{date.year}_{date.month}_{date.day}"
+
+
+def day_urls(date, end_date):
+
+    while date <= end_date:
+        yield day_url(date)
+        date += datetime.timedelta(days=1)
 
 
 def get_page(url, headers=HEADERS):
@@ -32,37 +39,28 @@ def get_page(url, headers=HEADERS):
     return page
 
 
-def scan_months(url, period=730, xpaths=XPATHS, headers=HEADERS):
-    
-    months = get_page(url, headers).xpath(xpaths["months"])
-
-    day_urls = []
-    for month in months:
-        month_days = get_page(month.attrib["href"]).xpath(xpaths["days"])
-
-        for day in month_days:
-            day_urls.append(day.attrib["href"])
-
-            if len(day_urls) == period:
-                return day_urls
-
-
-def scan_day(day_url, xpaths=XPATHS, headers=HEADERS):
+def scan_day(day_url, patience=3, xpaths=XPATHS, headers=HEADERS):
 
     article_urls = []
     article_titles = []
-    while 1:
-        print(f"Scanning {day_url}...")
+    while tries > patience:
+        print(f"Scanning {day_url}")
         page = get_page(day_url)
-        next_elements = page.xpath(xpaths["next"])
-        if not next_elements:
-            return article_urls, article_titles
-        day_url = next_elements[-1].attrib["href"]
-
         articles = page.xpath(xpaths["articles"])
         for article in articles:
             article_urls.append(article.attrib["href"])
             article_titles.append(article.text)
+
+        next_elements = page.xpath(xpaths["next"])
+        if not next_elements:
+            print("\tRETRYING")
+            tries += 1
+            continue
+        tries = 0
+        next_element = next_elements[-1]
+        if next_element.text != "Next":
+            return article_urls, article_titles
+        day_url = next_element.attrib["href"]
 
 
 def scan_article(url, xpaths=XPATHS, headers=HEADERS):
