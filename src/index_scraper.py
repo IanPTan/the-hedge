3f7174f82107a3982abacb6893b428e64389a831
IanPatrickMTan <ianparicktan@gmail.com>
2024-12-28 12:07:40 -0500

    more or less done the index scraper

---
+++ src/index_scraper.py
@@ -0,0 +1,27 @@
+from ticker import *
+
+
+start_date = dt.date(2023, 2, 1)
+end_date = dt.date(2024, 12, 26)
+
+start_days = date_to_days(start_date)
+end_days = date_to_days(end_date)
+
+all_days = range(start_days, end_days + 1)
+
+start = time()
+article_urls, article_titles, urls, statuses, counts = scan_days(all_days, 1)
+dur = time() - start
+
+success = 0
+for status in statuses:
+    if status[0] == "S":
+        success += 1
+
+data = pd.DataFrame({"url": article_urls, "title": article_titles})
+data.to_csv("all_articles.csv", index=False)
+
+logs = pd.DataFrame({"url": urls, "status": statuses, "count": counts})
+logs.to_csv("logs.csv", index=False)
+
+print(f"Took {dur:.4f} seconds to scrape {len(urls)} articles, {success}/{end_days - start_days + 1} days succeeded")
--- src/ticker.py
+++ src/ticker.py
@@ -4,8 +4,11 @@ import pandas as pd
 from lxml import html
 from time import time, sleep
 import datetime as dt
+import yfinance as yf
+from tqdm import tqdm
 
 
+# 19632
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
 INDEX_URL = "https://finance.yahoo.com/sitemap"
 XPATHS = {
@@ -14,7 +17,8 @@ XPATHS = {
         "articles": "/html/body/div[1]/div/main/div/div/div/ul/li/a",
         "next": "/html/body/div[1]/div/main/div/div/div/div[2]/a",
         "tickers": "/html/body/div[2]/main/section/section/section/article/div/div[1]/div[3]/div[1]/div/div/div/div/div/a/div/span",
-        "time": "/html/body/div[2]/main/section/section/section/article/div/div[1]/div[2]/div[1]/div/div[2]/time"
+        "time": "/html/body/div[2]/main/section/section/section/article/div/div[1]/div[2]/div[1]/div/div[2]/time",
+        "text": "/html/body/div[2]/main/section/section/section/article/div/div[1]/div[3]/div[2]/p"
         }
 EPOCH = dt.date(1970, 1, 1)
 days_to_date = lambda days: EPOCH + dt.timedelta(days=days)
@@ -41,57 +45,65 @@ def get_page(url, headers=HEADERS):
     return page
 
 
-def scan_day(days, patience=3, xpaths=XPATHS, headers=HEADERS):
+def scan_day(days, patience=10, wait=10, xpaths=XPATHS, headers=HEADERS):
 
-    print(f"Scanning {day_url}")
     date = days_to_date(days)
     day_url = get_day(date)
+    #print(f"Scanning {day_url}")
 
     article_urls = []
     article_titles = []
     tries = 0
     while tries < patience:
+        sleep(tries * wait)
+
         try:
             page = get_page(day_url)
         except ValueError as e:
-            print(f"\tRETRYING {e}")
+            #print(f"\tRETRYING {e}")
             tries += 1
             continue
             
         articles = page.xpath(xpaths["articles"])
         for article in articles:
-            article_urls.append(article.attrib["href"])
-            article_titles.append(article.text)
+            if "href" in article.attrib:
+                article_urls.append(article.attrib["href"])
+                article_titles.append(article.text)
 
         next_elements = page.xpath(xpaths["next"])
         if not next_elements:
-            print("\tRETRYING")
+            #print("\tRETRYING")
             tries += 1
             continue
         tries = 0
 
         next_element = next_elements[-1]
         if next_element.text != "Next":
-            return article_urls, article_titles, f"S {day_url}"
+            #print(f"Successfully scraped {len(article_urls)} articles from {day_url}")
+            return article_urls, article_titles, day_url, 1
         day_url = next_element.attrib["href"]
 
-    print("\tGIVING UP")
-    return article_urls, article_titles, f"F {day_url}"
+    #print("\tGIVING UP")
+    return article_urls, article_titles, day_url, 0
 
 
-def scan_days(all_days, workers=16, patience=3, xpaths=XPATHS, headers=HEADERS):
+def scan_days(all_days, workers=16, patience=10, wait=10, xpaths=XPATHS, headers=HEADERS):
 
     article_urls = []
     article_titles = []
+    urls = []
     statuses = []
+    counts = []
     with cf.ThreadPoolExecutor(max_workers=workers) as executor:
-        futures = [executor.submit(scan_day, days, patience, xpaths, headers) for days in all_days]
-        for future in cf.as_completed(futures):
-            day_urls, day_titles, status = future.result()
+        futures = [executor.submit(scan_day, days, patience, wait, xpaths, headers) for days in all_days]
+        for future in tqdm(cf.as_completed(futures), total=len(futures), desc="Scraping...", unit="days"):
+            day_urls, day_titles, url, status = future.result()
             article_urls += day_urls
             article_titles += day_titles
+            urls.append(url
             statuses.append(status)
-    return article_urls, article_titles, statuses
+            counts.append(len(day_urls))
+    return article_urls, article_titles, url, statuses, counts
 
 
 def scan_article(url, xpaths=XPATHS, headers=HEADERS):
@@ -104,17 +116,11 @@ def scan_article(url, xpaths=XPATHS, headers=HEADERS):
     return tickers, time
 
 
-if __name__ == "__main__":
-    start_date = dt.date(2024, 10, 1)
-    start_days = date_to_days(start_date)
-    end_date = dt.date(2024, 12, 10)
-    end_days = date_to_days(end_date)
-    all_days = range(start_days, end_days + 1)
-    start = time()
-    urls, titles, statuses = scan_days(all_days, 2)
-    dur = time() - start
-    success = 0
-    for status in statuses:
-        if status[0] == "S":
-            success += 1
-    print(F"Took {dur:.4f} seconds to scrape {len(urls)} articles, {sum(statuses)}/{end_days - start_days + 1} days succeeded")
+def yfin_scan(ticker, interval="30m", price_cols=["Open", "Close", "High", "Low", "Volume"]):
+
+    ticker = yf.Ticker(ticker)
+    prices = ticker.history(interval=interval)[price_cols]
+    prices["time"] = prices.index.astype(int) // 10 ** 9
+    prices.rename(columns={name: name.lower() for name in price_cols}, inplace=True)
+    
+    return prices
